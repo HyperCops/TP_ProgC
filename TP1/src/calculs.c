2f6353542b559f1a6cc54defa0f195469fdd5823
HyperCops <anthonydealmeidamajor@gmail.com>
2026-01-29 11:39:20 +0100

    Implement basic calculator with multiple operations

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,45 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 12;
+    int num2 = 5;
+    char op = '&'; // Changez ceci pour tester +, -, *, /, %, |, ~
+
+    printf("Operation : %d %c %d\n", num1, op, num2);
+
+    switch (op) {
+        case '+':
+            printf("Resultat : %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Resultat : %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Resultat : %d\n", num1 * num2);
+            break;
+        case '/':
+            if(num2 != 0) printf("Resultat : %d\n", num1 / num2);
+            else printf("Erreur: Division par zero\n");
+            break;
+        case '%':
+            if(num2 != 0) printf("Resultat : %d\n", num1 % num2);
+            else printf("Erreur: Division par zero\n");
+            break;
+        case '&':
+            printf("Resultat (ET binaire) : %d\n", num1 & num2);
+            break;
+        case '|':
+            printf("Resultat (OU binaire) : %d\n", num1 | num2);
+            break;
+        case '~':
+            // Le NON binaire est unaire, il ne s'applique qu'à num1 ici
+            printf("Resultat (NON binaire sur num1) : %d\n", ~num1);
+            break;
+        default:
+            printf("Operateur inconnu.\n");
+    }
+
+    return 0;
+}
